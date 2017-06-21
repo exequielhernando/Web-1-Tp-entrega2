b8eec11ee1442cb9f79eddf0ac9df300a6855648
Exequiel Hernando <exequielhernando@gmail.com>
2017-06-21 15:02:54 -0300

    Agregados los comentarios y la variaciones

--- juego/js/main.js
+++ juego/js/main.js
@@ -1,6 +1,8 @@
 "use strict";
+//Código de inicialización de eventos
 $("document").ready(function() {
-
+//Declaracion de la clase jugador y los atributos requeridos(La imagen correspondiente al jugadorUsuario,
+//el crédito necesario para jugar, la eleccion del mismo, el contador de partidas ganadas, y la apuesta necesaria para jugar)
   class jugadorUsuario {
     constructor(idimg) {
       this.idimg = idimg;
@@ -10,29 +12,35 @@ $("document").ready(function() {
       this.apuesta=5;
 
     }
+    //Elegimos la jugada mediante el atributo correspondiente al boton que apretemos y tomamos la url de la imagen para luego poder modificarla
     elegirJugada(eleccionJugada){
       this.eleccionJugada=eleccionJugada;
       $(this.idimg).attr("src",'images/'+this.eleccionJugada+'.jpg');
     }
+    //Actualizamos el contador para el jugador que gane la partida y tambien colocamos el metodo para modificarlo en el html.
     actualizarContador(){
       this.contador++;
       $(".js-partidasUsuario").html(this.contador);
       console.log("entro");
     }
+    //Esta funcion nos descuenta la apuesta del credito del jugador y tambien lo modifica en el html.
     apostar(){
       this.credito-=this.apuesta;
       $(".js-credito").html(this.credito);
     }
+    //Actualizamos el credito en caso de que el jugador gane.
     actualizarCredito(){
       this.credito+=(this.apuesta*2);
       $(".js-credito").html(this.credito);
     }
+    //Actualizamos el credito en caso de que el jugador gane tomando en cuenta la variacion que nos duplica el valor obtenido.
     spockfriendly(){
       this.credito+=(this.apuesta*4);
       $(".js-credito").html(this.credito);
     }
 
   }
+  //Declaramos la clase jugador pc y sus atributos.
   class jugadorPc {
     constructor(idimg) {
       this.idimg=idimg;
@@ -40,6 +48,7 @@ $("document").ready(function() {
       this.contador=0;
 
     }
+    //Declaramos la variacion modosheldon para que la eleccion sea distinta.
     modosheldon(eleccionJugada){
       let eleccion=Math.floor((Math.random()*2)+1);
         if (eleccion==1){
@@ -50,6 +59,7 @@ $("document").ready(function() {
         }
         $(this.idimg).attr("src",'images/'+this.eleccionJugada+'.jpg');
     }
+    //Declaramos una funcion para que eliga una jugada al azar y tambien que nos modifique la imagen en el html.
     elegirJugada(eleccionJugada){
       let eleccion = Math.floor((Math.random()*5)+1);
         if (eleccion == 1) {
@@ -66,33 +76,37 @@ $("document").ready(function() {
         }
         $(this.idimg).attr("src",'images/'+this.eleccionJugada+'.jpg');
     }
+    //Actualiza el contador de las partidas ganadas.
     actualizarContador(){
       this.contador++;
       $(".js-partidasPc").html(this.contador);
       console.log("entro");
     }
   }
+  //Declaramos la clase donde sucede el juego
   class juego {
+    //Creamos los objetos para empezar a jugar y tomamos la ubicacion de su imagen.
     constructor() {
 
     this.usuario= new jugadorUsuario(".imagen_humano");
     this.pc= new jugadorPc(".imagen_maquina");
-    }
+  }
+
     jugar(jugadahumano){
-      if(this.usuario.credito>=this.usuario.apuesta){
-          this.usuario.apostar();
-          if ($(".js-sheldon:checked").val()=="on") {
+      if(this.usuario.credito>=this.usuario.apuesta){//Preguntamos si el jugador usuario tiene credito suficiente para jugar.
+          this.usuario.apostar();                   //En caso de que tenga credito, apuesta.
+          if ($(".js-sheldon:checked").val()=="on") {//Preguntamos si el checkbox esta checkeado para utilizar la variacion modosheldon.
             this.pc.modosheldon();
           }
-          else {
+          else {                                    //Sino que eliga una jugada al azar.
             this.pc.elegirJugada();
           }
 
-          this.usuario.elegirJugada(jugadahumano);
-          if(this.usuario.eleccionJugada === this.pc.eleccionJugada){
+          this.usuario.elegirJugada(jugadahumano);   //Toma el valor que eligio el usuario.
+          if(this.usuario.eleccionJugada === this.pc.eleccionJugada){//Los compara para ver si hay empate.
             alert("empataste");
             console.log(alert);
-          }else if(this.usuario.eleccionJugada == "Piedra" && this.pc.eleccionJugada == "Tijera"
+          }else if(this.usuario.eleccionJugada == "Piedra" && this.pc.eleccionJugada == "Tijera"//Sino, cualquiera de estas opciones son ganador.
                         ||
             this.usuario.eleccionJugada == "Piedra" && this.pc.eleccionJugada == "Lagarto"
                           ||
@@ -120,12 +134,12 @@ $("document").ready(function() {
               this.usuario.actualizarContador();
             }
             else {
-              this.usuario.actualizarCredito();
+              this.usuario.actualizarCredito(); //Actualiza el contador y el credito.
               this.usuario.actualizarContador();
             }
 
           }
-          else
+          else//Sino dice que el usuario perdio y actualiza el contador de la pc
           {
             alert("Perdiste");
             console.log(alert);
@@ -135,13 +149,13 @@ $("document").ready(function() {
       }
       else
       {
-        alert("No dispone de mas credito para jugar");
+        alert("No dispone de mas credito para jugar"); //En caso de que no tenga mas credito para jugar le avisa mediante un alert.
       };
     };
   }
-  let jugada = new juego();
-  $(".eleccion").on('click', function() {
-
+  let jugada = new juego();                     //Crea un objeto juego para comenzar a jugar.
+  $(".eleccion").on('click', function() {       //Llama mediante el atributo id eleccion y pregunta si hizo click para ejecutar
+                                                //siguiente funcion.    
         jugada.jugar($(this).attr("id"));
     });
 });
